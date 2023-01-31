7cfec6ea32b0ea8d66778f7d40e865150b86c4ad
ik3 <aybikeboran7140@gmail.com>
2023-02-01 02:08:10 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using PasswordGenerator.Console;
+
+string password;
+
+RandomPasswordGenerator randomPasswordGenerator = new();
+
+string[] questionArray = {"Include Numbers? (y/n)", "Include upperCase? (y/n)", "Include lowerCase ? (y / n)", "Include specialChar? (y/n)", "What is the lenght of password?" };
+bool[] valueArray= new bool[4];
+int lenght=0;
+for(int i = 0; i<questionArray.Length; i++)
+    {
+    if (i == questionArray.Length-2)
+    {
+        Console.WriteLine(questionArray[i]);
+        var _lenght = Console.ReadLine();
+        lenght=Convert.ToInt32(_lenght);
+    }
+    else { 
+        Console.WriteLine(questionArray[i]);
+        string _value = Console.ReadLine();
+        bool value = inputController(_value);
+        valueArray[i] = value;}
+    }
+
+password = randomPasswordGenerator.GeneratePassword(valueArray[0], valueArray[1], valueArray[2], valueArray[3], lenght);
+
+Console.WriteLine($"your password is: {password}");
+Boolean inputController(string input) {
+    input.ToLower();
+
+    switch (input)
+    {
+        case "y":
+        case "yes":
+            return true;
+        case "n":
+        case "no":
+            return false;
+        default:
+            Console.WriteLine("answer must be like y/n or yes/no");
+            return false;
+    }
+
+};
+
---
+++ RandomString.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace PasswordGenerator.Console
+{
+    class RandomPasswordGenerator
+    {
+        const string lowerCase = "qwertyuopasdfghjklizxcvbnm";
+        const string upperCase = "QWERTYUIOPASDFGHJKLZXCVBNM";
+        const string numbers = "0123456789";
+        const string specials = "!'^+%&/()=?_.€@~$*-";
+        public string GeneratePassword(bool useNumbers, bool useUppercase, bool useLowercase, bool useSpecial,
+            int passwordSize)
+        {
+            char[] _password = new char[passwordSize];
+            string charSet = "";
+            System.Random _random = new Random();
+
+            if (useLowercase) charSet += lowerCase;
+            if (useUppercase) charSet += upperCase;
+            if (useNumbers) charSet += numbers;
+            if (useSpecial) charSet += specials;
+            if (charSet.Length==0) return charSet;
+
+            for (int i = 0; i < passwordSize; i++)
+            {
+                _password[i] = charSet[_random.Next(charSet.Length - 1)];
+            }
+
+            return String.Join(null, _password);
+        }
+    }
+}
