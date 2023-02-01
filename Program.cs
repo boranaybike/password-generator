fcdfb0facb7b9ca55ab8236205b615e1ae964b65
ik3 <aybikeboran7140@gmail.com>
2023-02-01 22:55:34 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,29 +1,25 @@
 ﻿using PasswordGenerator.Console;
 
 string password;
-
 RandomPasswordGenerator randomPasswordGenerator = new();
+Console.WriteLine("**********************************\nWELCOME TO THE PASSWORD GENERATOR\n**********************************");
 
-string[] questionArray = {"Include Numbers? (y/n)", "Include upperCase? (y/n)", "Include lowerCase ? (y / n)", "Include specialChar? (y/n)", "What is the lenght of password?" };
+string[] questionArray = {"Include Numbers? (y/n)", "Include upperCase? (y/n)", "Include lowerCase ? (y/n)", "Include specialChar? (y/n)"};
 bool[] valueArray= new bool[4];
 int lenght=0;
-for(int i = 0; i<questionArray.Length; i++)
-    {
-    if (i == questionArray.Length-2)
-    {
-        Console.WriteLine(questionArray[i]);
-        var _lenght = Console.ReadLine();
-        lenght=Convert.ToInt32(_lenght);
-    }
-    else { 
+
+for(int i = 0; i<questionArray.Length; i++) {
         Console.WriteLine(questionArray[i]);
         string _value = Console.ReadLine();
         bool value = inputController(_value);
-        valueArray[i] = value;}
+        valueArray[i] = value;
     }
 
-password = randomPasswordGenerator.GeneratePassword(valueArray[0], valueArray[1], valueArray[2], valueArray[3], lenght);
+Console.WriteLine("What is the lenght of password?");
+var _lenght = Console.ReadLine();
+lenght = Convert.ToInt32(_lenght);
 
+password = randomPasswordGenerator.GeneratePassword(valueArray[0], valueArray[1], valueArray[2], valueArray[3], lenght);
 Console.WriteLine($"your password is: {password}");
 Boolean inputController(string input) {
     input.ToLower();
